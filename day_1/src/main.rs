f5a1d62f35d8a7af6d73a1a10d8b6e20d2c9e8e7
Gary Clark <gclark@xccelerated.io>
2022-12-22 13:04:30 +0100

    day 1 part 2

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -6,21 +6,30 @@ fn main() {
     let data = fs::read_to_string("data.txt").expect("error reading file");
 
     // elves are split by 2 consecutive new lines
-    let calorie_rich_elf = data
+    let mut elves = data
         .split("\n\n")
 
         // for each elf, snacks are split by one new line
         // coerce each snack to an integer
         // sum up the amounts of the snack calories per elf
-        // grab the elf with the most calories
+        
         .map(|elf| {
             elf
                 .split("\n")
                 .map(|snack| snack.parse::<i32>().expect("error parsing string to integer"))
                 .sum::<i32>()
         })
-        .max().expect("no values available to reduce to a max");
+        .collect::<Vec<i32>>();
 
+        elves.sort();
 
-        println!("{:?}", calorie_rich_elf)
+        let ordered_elves = elves
+            .iter()
+            .rev()
+            .collect::<Vec<&i32>>();
+
+        let top_three_sum = ordered_elves[0..3].to_owned().into_iter().sum::<i32>();
+        let top_elf = &ordered_elves[0];
+        
+        println!("top elf is {:?}, but to be safe lets get how much the top three have which is {:?}", top_elf, top_three_sum)
 }
